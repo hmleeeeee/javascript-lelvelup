2e1c5742bad60009da874533546dedd1caacf4bd
hmleeeeee <hmslow21@gmail.com>
2023-03-15 10:43:12 +0900

    문자 - slice

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const str = '0123'
+const str = 'Hello world!'
 
-console.log(str.length)
-// length -> 문자 개수 반환
+console.log(str.slice(0, 3))
+
+// slice -> 문자열의 일부를 추출하면서 새로운 문자열을 반환
+// endIndex - 0부터 시작하는 추출 종료점 인덱스로 그 직전까지 추출
