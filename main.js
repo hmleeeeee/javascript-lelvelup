a4757fb86951c4842eb7cf0a6a0452a47020a7fb
hmleeeeee <hmslow21@gmail.com>
2023-03-15 10:35:30 +0900

    문자 - indexOf

---
+++ main.js
@@ -0,0 +1,8 @@
+const result = 'Hello world!'.indexOf('world')
+console.log(result)
+
+// String.prototype.indexOf()
+
+const result2 = 'Hello world!'.indexOf('Hyoki')
+console.log(result2)
+// 일치하는 값이 없으면 -1 반환
