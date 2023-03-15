87c9ea177e305a49ff5898eccdeaeb6dbae20441
hmleeeeee <hmslow21@gmail.com>
2023-03-15 10:39:31 +0900

    문자 - length

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-const result = 'Hello world!'.indexOf('world')
-console.log(result)
+const str = '0123'
 
-// String.prototype.indexOf()
-
-const result2 = 'Hello world!'.indexOf('Hyoki')
-console.log(result2)
-// 일치하는 값이 없으면 -1 반환
+console.log(str.length)
+// length -> 문자 개수 반환
