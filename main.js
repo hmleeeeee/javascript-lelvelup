0ac57f576803edae90c9eed8f95d7ed5a3db83bc
hmleeeeee <hmslow21@gmail.com>
2023-03-15 17:29:07 +0900

    수학 객체 - abs, min, max, ceil, floor, round, random

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-const pi = 3.14159265358979
-console.log(pi)
-
-const str = pi.toFixed(2) // toFixed() -> 소수점 몇번째 자리까지 유지할지
-console.log(str)
-console.log(typeof str)
-
-const integer = parseInt(str) // 정수 반환
-const float = parseFloat(str) // 소수점 유지하면서 숫자 반환
-console.log(integer)
-console.log(float)
-console.log(typeof integer, typeof float)
+console.log('abs: ', Math.abs(-12))
+// 특정한 숫자의 절대값을 반환, 음수 제거된 양수 출력
+console.log('min: ', Math.min(2, 8))
+// 인수로 들어온 숫자 데이터 중 가장 작은 값을 반환
+console.log('max: ', Math.max(2, 8))
+// 인수로 들어온 숫자 데이터 중 가장 큰 값을 반환
+console.log('ceil: ', Math.ceil(3.14))
+// 인수로 들어온 숫자 데이터 중 올림 처리
+console.log('floor: ', Math.floor(3.14))
+// 인수로 들어온 숫자 데이터 중 내림 처리
+console.log('round: ', Math.round(3.14))
+// 인수로 들어온 숫자 데이터 중 반올림 처리
+console.log('random: ', Math.random())
+// 랜덤한 숫자를 반환
