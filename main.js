6347bf6b3d1ec029ef37fd4092c85cfbc4e806ff
hmleeeeee <hmslow21@gmail.com>
2023-03-15 17:20:42 +0900

    숫자 - parseInt(), parseFloat()

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-const str = '    Hello world     '
+const pi = 3.14159265358979
+console.log(pi)
 
-console.log(str.trim())
+const str = pi.toFixed(2) // toFixed() -> 소수점 몇번째 자리까지 유지할지
+console.log(str)
+console.log(typeof str)
 
-//trim -> 특정한 문자 데이터에서 앞 뒤 공백을 제거
+const integer = parseInt(str) // 정수 반환
+const float = parseFloat(str) // 소수점 유지하면서 숫자 반환
+console.log(integer)
+console.log(float)
+console.log(typeof integer, typeof float)
